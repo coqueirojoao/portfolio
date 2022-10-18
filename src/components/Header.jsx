032be5bd49f83b6298b0f7f98b76e770c158658e
João Coqueiro <jaaum.p95@gmail.com>
2022-10-18 12:51:14 -0300

    Cria header e instala Tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <li>Home</li>
+        <li>Projetos</li>
+        <li>Sobre mim</li>
+      </nav>
+    </header>
+  );
+}
